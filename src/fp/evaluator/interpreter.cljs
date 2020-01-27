861cc7f14d68b6fbcab4496478523100ba9b718b
Benjamin Buccianti <benjamin@buccianti.dev>
2020-01-27 14:56:48 -0300

    minor refactoring of the intepreter

--- src/fp/evaluator/interpreter.cljs
+++ src/fp/evaluator/interpreter.cljs
@@ -2,28 +2,36 @@
   (:require [clojure.core.match :refer [match]]))
 
 (defn evaluate [parsed-map]
-  (let [apli (get parsed-map :application)
-        operator (get-in parsed-map [:application :operator])
+  (let [operator (get-in parsed-map [:application :operator])
         operand (get-in parsed-map [:application :operand])]
     (match [operator]
       [{:type :number}]
       (get (:sequence operand) (dec (:val operator)))
 
       [(n :guard #(boolean (re-matches #"\dr" (:string operator))))]
-      (let [vctr (vec (reverse (:sequence operand)))
-            i (js/parseInt (first (:string operator)))]
+      (let [vctr (-> operand :sequence reverse vec)
+            i (-> operator :string first js/parseInt)]
         (get vctr (dec i)))
 
       [{:type :symbol}]
       (case (:string operator)
-        "tl" {:sequence (vec (rest (:sequence operand)))}
-        "tlr" {:sequence (-> (:sequence operand) reverse rest reverse vec)}
-        "id" operand
-        "atom" {:type :bool :val (not (contains? operand :sequence))}
-        "eq" (let [sqc (:sequence operand)]
-               (if (or (< (count sqc) 2) (> (count sqc) 2))
-                 {:string "⊥" :type :undefined}
-                 {:type :bool :val (= (-> sqc first :string)
-                                      (-> sqc second :string))})))
+        "tl"
+        {:sequence (vec (rest (:sequence operand)))}
+
+        "tlr"
+        {:sequence (-> (:sequence operand) reverse rest reverse vec)}
+
+        "id"
+        operand
+
+        "atom"
+        {:type :bool :val (not (contains? operand :sequence))}
+
+        "eq"
+        (let [sqc (:sequence operand)]
+          (if (not= 2 (count sqc))
+            {:string "⊥" :type :undefined}
+            {:type :bool :val (= (-> sqc first :string)
+                                 (-> sqc second :string))})))
 
       :else parsed-map)))
