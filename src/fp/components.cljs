fa3c25028660ec34caf4a48082ada3e8f65ecadc
Benjamin Buccianti <benjamin@buccianti.dev>
2020-01-28 17:44:40 -0300

    client ui can call evaluator now

--- src/fp/components.cljs
+++ src/fp/components.cljs
@@ -1,21 +1,32 @@
 (ns fp.components
   (:require
-   [fp.semantic :as ui]))
+   [fp.semantic :as ui]
+   [fp.evaluator.parser :refer [parse]]
+   [fp.evaluator.interpreter :refer [evaluate]]
+   [fp.evaluator.stringify :refer [to-string]]))
+
+(defn handle-action [in out]
+  (do
+    (swap! out conj (-> @in parse evaluate to-string))
+    (reset! in "")))
+
+(defn handle-key-pressed [e in out]
+  (when (= "Enter" (.-key e))
+    (handle-action in out)))
 
 (defn input-bar [in out]
   [:> ui/container
    {:style {:margin "20px 0"}}
    [:> ui/input
-    {:placeholder "Inserte una expresión..."
+    {:placeholder "Insertá una expresión!"
      :fluid true
      :size "huge"
      :value @in
-     :onChange (fn [e v] (reset! in (.-value v)))
+     :onKeyPress #(handle-key-pressed % in out)
+     :onChange (fn [_ v] (reset! in (.-value v)))
      :action
      {:content "Evaluar"
-      :on-click #(do
-                   (swap! out conj @in)
-                   (reset! in ""))}}]])
+      :onClick #(handle-action in out)}}]])
 
 (defn output-segments [out]
   [:> ui/container
