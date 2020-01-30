569dc4571a68d0c4a9e93e25f2c9e6505e49119b
Benjamin Buccianti <benjamin@buccianti.dev>
2020-01-30 16:42:47 -0300

    refactor

--- src/fp/evaluator/interpreter.cljs
+++ src/fp/evaluator/interpreter.cljs
@@ -1,139 +1,141 @@
 (ns fp.evaluator.interpreter
   (:require [clojure.core.match :refer [match]]))
 
+(defn evaluate-application [op operand]
+  (cond
+    ;; SELECTORS
+    (= op "tl")
+    {:sequence (vec (rest (:sequence operand)))}
+
+    (= op "tlr")
+    {:sequence (-> (:sequence operand) reverse rest reverse vec)}
+
+    (= op "id")
+    operand
+
+    ;; PREDICATES
+    (= op "atom")
+    {:type :bool :val (not (contains? operand :sequence))}
+
+    (and (= op "eq") (= 2 (count (:sequence operand))))
+    {:type :bool
+     :val (apply = (map :string (:sequence operand)))}
+
+    (= op "null")
+    {:type :bool :val (= (:type operand) :empty)}
+
+    ;; ARITHMETICS
+    (and (or (= op "+") (= op "-") (= op "×"))
+         (= 2 (count (:sequence operand)))
+         (every? (partial = :number) (map :type (:sequence operand))))
+    {:type :number
+     :val (apply (case op
+                   "+" +
+                   "-" -
+                   "×" *)
+                 (map :val (:sequence operand)))}
+
+    (and (= op "÷") (= 2 (count (:sequence operand)))
+         (every? (partial = :number) (map :type (:sequence operand)))
+         (not= 0 (:val (last (:sequence operand)))))
+    {:type :number :val (apply / (map :val (:sequence operand)))}
+
+    ;; LOGIC
+    (and (or (= op "and") (= op "or"))
+         (= 2 (count (:sequence operand)))
+         (every? (partial = :bool) (mapv :type (:sequence operand))))
+    {:type :bool
+     :val ((case op
+             "and" every?
+             "or" some)
+           identity (mapv :val (:sequence operand)))}
+
+    (and (= op "not") (= :bool (:type operand)))
+    {:type :bool :val (not (:val operand))}
+
+    ;; SEQUENCES
+    (and (= op "length") (not= :symbol (:type operand)))
+    {:type :number
+     :val (if (= (:type operand) :empty)
+            0
+            (count (:sequence operand)))}
+
+    (and (= op "reverse") (:sequence operand))
+    {:sequence (reverse (:sequence operand))}
+
+    (and (= op "trans") (:sequence operand))
+    (let [v (map :sequence (:sequence operand))]
+      {:sequence (mapv #(into {} {:sequence %})
+                       (apply mapv vector v))})
+
+    (and (= op "distl") (:sequence operand))
+    (let [left (first (:sequence operand))
+          right (:sequence (second (:sequence operand)))]
+      {:sequence (mapv #(into {} {:sequence %})
+                       (mapv vector (repeat left) right))})
+
+    (and (= op "distr") (:sequence operand))
+    (let [left (:sequence (first (:sequence operand)))
+          right (second (:sequence operand))]
+      {:sequence (mapv #(into {} {:sequence %})
+                       (mapv vector left (repeat right)))})
+
+    (= op "apndl")
+    (let [left (first (:sequence operand))
+          right (:sequence (second (:sequence operand)))]
+      {:sequence (into [left] right)})
+
+    (= op "apndr")
+    (let [left (:sequence (first (:sequence operand)))
+          right (second (:sequence operand))]
+      {:sequence (reverse (into [right] (reverse left)))})
+
+    (and (= op "rotl") (or (= :empty (:type operand)) (:sequence operand)))
+    (if (= :empty (:type operand))
+      {:type :empty}
+      {:sequence (conj (vec (rest (:sequence operand)))
+                       (first (:sequence operand)))})
+
+    (and (= op "rotr") (or (= :empty (:type operand)) (:sequence operand)))
+    (if (= :empty (:type operand))
+      {:type :empty}
+      {:sequence (into [(last (:sequence operand))]
+                       (butlast (:sequence operand)))})
+
+    :else {:type :undefined}))
+
 (defn evaluate [parsed-map]
-  (let [operator (get-in parsed-map [:application :operator])
-        operand (get-in parsed-map [:application :operand])]
-    (declare sqc) ;; avoid awful logs for use of undeclared
-    (match [parsed-map]
-      [{:construction cnst}]
-      {:sequence
-       (mapv (fn [f o]
-               (evaluate {:application {:operator f :operand o}}))
-             (:functions cnst) (repeat (:operand cnst)))}
+  (match [parsed-map]
+    [{:construction cnst}]
+    {:sequence
+     (mapv (fn [f o]
+             (evaluate {:application {:operator f :operand o}}))
+           (:functions cnst) (repeat (:operand cnst)))}
 
-      [{:composition cmpst}]
-      (reduce (fn [sqc f]
-                (evaluate {:application {:operator f
-                                         :operand sqc}}))
-              (:operand cmpst) (:functions cmpst))
+    [{:composition cmpst}]
+    (reduce (fn [sqc f]
+              (evaluate {:application {:operator f
+                                       :operand sqc}}))
+            (:operand cmpst) (:functions cmpst))
 
-      [{:application appli}]
-      (let [operator (get appli :operator)
-            operand (get appli :operand)]
-        (match [operator operand]
-          [_ {:type :undefined}]
-          {:type :undefined}
+    [{:application appli}]
+    (let [operator (get appli :operator)
+          operand (get appli :operand)]
+      (match [operator operand]
+        [_ {:type :undefined}]
+        {:type :undefined}
 
-          [{:type :number} _]
-          (get (:sequence operand) (dec (:val operator)))
+        [{:type :number} _]
+        (get (:sequence operand) (dec (:val operator)))
 
-          [(n :guard #(boolean (re-matches #"\dr" (:string operator)))) _]
-          (let [vctr (-> operand :sequence reverse vec)
-                i (-> operator :string first js/parseInt)]
-            (get vctr (dec i)))
+        [(n :guard #(boolean (re-matches #"\dr" (:string operator)))) _]
+        (let [vctr (-> operand :sequence reverse vec)
+              i (-> operator :string first js/parseInt)]
+          (get vctr (dec i)))
 
-          [{:type :symbol :string op} (:or {:sequence sqc} _)]
-          (cond
-            ;; SELECTORS
-            (= op "tl")
-            {:sequence (vec (rest sqc))}
+        [{:type :symbol :string op} (:or {:sequence sqc} _)]
+        (evaluate-application op operand)
 
-            (= op "tlr")
-            {:sequence (-> sqc reverse rest reverse vec)}
+        :else "Error"))
 
-            (= op "id")
-            operand
-
-            ;; PREDICATES
-            (= op "atom")
-            {:type :bool :val (not (contains? operand :sequence))}
-
-            (and (= op "eq") (= 2 (count sqc)))
-            {:type :bool
-             :val (apply = (map :string sqc))}
-
-            (= op "null")
-            {:type :bool :val (= (:type operand) :empty)}
-
-            ;; ARITHMETICS
-            (and (or (= op "+") (= op "-") (= op "×"))
-                 (= 2 (count sqc))
-                 (every? (partial = :number) (map :type sqc)))
-            {:type :number
-             :val (apply (case op
-                           "+" +
-                           "-" -
-                           "×" *)
-                         (map :val sqc))}
-
-            (and (= op "÷") (= 2 (count sqc))
-                 (every? (partial = :number) (map :type sqc))
-                 (not= 0 (:val (last sqc))))
-            {:type :number :val (apply / (map :val sqc))}
-
-            ;; LOGIC
-            (and (or (= op "and") (= op "or"))
-                 (= 2 (count sqc))
-                 (every? (partial = :bool) (mapv :type sqc)))
-            {:type :bool
-             :val ((case op
-                     "and" every?
-                     "or" some)
-                   identity (mapv :val sqc))}
-
-            (and (= op "not") (= :bool (:type operand)))
-            {:type :bool :val (not (:val operand))}
-
-            ;; SEQUENCES
-            (and (= op "length") (not= :symbol (:type operand)))
-            {:type :number
-             :val (if (= (:type operand) :empty)
-                    0
-                    (count sqc))}
-
-            (and (= op "reverse") sqc)
-            {:sequence (reverse sqc)}
-
-            (and (= op "trans") sqc)
-            (let [v (map :sequence sqc)]
-              {:sequence (mapv #(into {} {:sequence %})
-                               (apply mapv vector v))})
-
-            (and (= op "distl") sqc)
-            (let [left (first sqc)
-                  right (:sequence (second sqc))]
-              {:sequence (mapv #(into {} {:sequence %})
-                               (mapv vector (repeat left) right))})
-
-            (and (= op "distr") sqc)
-            (let [left (:sequence (first sqc))
-                  right (second sqc)]
-              {:sequence (mapv #(into {} {:sequence %})
-                               (mapv vector left (repeat right)))})
-
-            (= op "apndl")
-            (let [left (first sqc)
-                  right (:sequence (second sqc))]
-              {:sequence (into [left] right)})
-
-            (= op "apndr")
-            (let [left (:sequence (first sqc))
-                  right (second sqc)]
-              {:sequence (reverse (into [right] (reverse left)))})
-
-            (and (= op "rotl") (or (= :empty (:type operand)) sqc))
-            (if (= :empty (:type operand))
-              {:type :empty}
-              {:sequence (conj (vec (rest sqc)) (first sqc))})
-
-            (and (= op "rotr") (or (= :empty (:type operand)) sqc))
-            (if (= :empty (:type operand))
-              {:type :empty}
-              {:sequence (into [(last sqc)] (butlast sqc))})
-
-            :else {:type :undefined})
-
-          :else "Error"))
-
-      :else "Error")))
+    :else "Error"))
