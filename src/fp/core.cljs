43ee08f3181bea3e94620829d0e9ca84be302189
Benjamin Buccianti <benjamin@buccianti.dev>
2020-02-22 19:47:20 -0300

    deleted components and put it in core

--- src/fp/core.cljs
+++ src/fp/core.cljs
@@ -3,7 +3,18 @@
    [goog.dom :as gdom]
    [reagent.core :as r]
    [fp.semantic :as ui]
-   [fp.components :refer [app]]))
+   [fp.components.input :refer [readline]]
+   [fp.components.output :refer [screen]]
+   [fp.components.config :refer [sidebar]]))
+
+(defn app []
+  (let [in (r/atom "")]
+    (fn []
+      [:> ui/container
+       {:id "container"}
+       [sidebar]
+       [screen]
+       [readline in]])))
 
 (defn mount-app []
   (when-let [el (gdom/getElement "app")]
